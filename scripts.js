6512e95ecdea62ee3e7808fadfa6b5dd106ac53a
Sowndharya M <sowndhu2000@gmail.com>
2024-11-15 16:19:18 +0530

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// Simple script for smooth scrolling when clicking anchor links
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
